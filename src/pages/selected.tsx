e842a211b9ef2296971a9010381534a42468acd0
Gustavo <gu.souza1998@yahoo.com.br>
2023-05-11 23:17:40 -0300

    update sizes again

--- src/pages/selected.tsx
+++ src/pages/selected.tsx
@@ -18,13 +18,13 @@ export default function Selected() {
     <div className={`
     p-5 m-5 flex flex-col gap-2
     `}>
-      <h1 className='m-2 text-red-500 flex text-justify justify-center text-6xl font-bold p-2 border-b-2 border-red-800'>Quadríceps</h1>
+      <h1 className='m-2 text-red-500 flex text-justify justify-center text-6xl font-bold p-2 border-b-2 border-red-800 w-screen'>Quadríceps</h1>
       <ul className='text-xl'>
       {exercisesArray.map((exercise) => (
         <li key={exercise} className={`
         exercise-${1} mb-3 text-xl justify-self text-center 
         `}>
-          <div className='display flex gap-10 p-5 m-3 justify-center items-center font-bold border-b-2 text-xl'>
+          <div className='display flex gap-8 m-3 p-5 ml-3 justify-center items-center font-bold border-b-2 border-b-zinc-500 text-2xl w-screen'>
             {exercise}
             <CheckboxWithModal />
             <CheckboxWithModal />
