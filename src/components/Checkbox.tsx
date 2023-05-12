ccbf84a7ea0d10e78a663cf6c40f0a287877e9b6
Gustavo <gu.souza1998@yahoo.com.br>
2023-05-11 23:32:21 -0300

    update sizes

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -6,7 +6,7 @@ type CheckboxProps = {
 
 const Checkbox = ({ onChange }: CheckboxProps) => {
   return <input
-    className='text-5xl bg-zinc-300  flex flex-row w-8 h-8 border rounded-full appearance-none border-black checked:bg-zinc-800' 
+    className=' bg-zinc-300  flex flex-row w-16 h-16 border rounded-full appearance-none border-black checked:bg-zinc-800' 
     type="checkbox"
     onChange={onChange} 
    />;
